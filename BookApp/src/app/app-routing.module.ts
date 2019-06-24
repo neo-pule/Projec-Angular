d8283798c14011900a2458169f8bb7c68e6ffa0c
neo-pule <malemelaneopule@gmail.com>
2019-06-24 04:44:09 -0700

    BookApp

---
+++ BookApp/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const routes: Routes = [{path:'', component: NavBarComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
